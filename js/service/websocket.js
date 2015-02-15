2df13e4ceb729308217e9def68f5ec640ae27e54
Yagi <me-me-@fout.jp>
2015-02-15 22:12:44 +0900

    fixed websocket url

--- js/service/websocket.js
+++ js/service/websocket.js
@@ -1,15 +1,27 @@
 var websocketServices = angular.module('websocketServices', []);
 
-websocketServices.factory('Poker', function() {
+websocketServices.factory('Poker', ['$location', function($location) {
     var ws;
+    var entryUrl = (function() {
+        var protocol = 'ws';
+        if ($location.protocol() == 'https') {
+            protocol = 'wss';
+        }
+        var port = '';
+        if ($location.port() != 80 && $location.port() != 443) {
+            port = ':' + $location.port();
+        }
+        return protocol + '://' + $location.host() + port + '/entry';
+    })();
+
     return {
         listen: function(onMessage) {
-            ws = new WebSocket("ws://localhost:8080/entry");
+            ws = new WebSocket(entryUrl);
             ws.onmessage = onMessage;
         },
         send: function(message) {
             ws.send(JSON.stringify(message));
         }
     };
-});
+}]);
 
