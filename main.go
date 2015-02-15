e2d4de3f0fdfe4658dab88e272b16201b87259ba
Yagi <me-me-@fout.jp>
2015-02-15 23:51:29 +0900

    delete test codes

--- main.go
+++ main.go
@@ -1,26 +1,16 @@
 package main
 
 import (
-	"net/http"
-	"fmt"
 	"./poker"
+	"fmt"
+	"net/http"
 )
 
-func helloHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "path : %s", r.URL.Path)
-}
-
 func main() {
-	// test
-	http.HandleFunc("/hello", helloHandler)
-
 	// websocket
 	server := poker.NewServer("/entry")
 	go server.Listen()
 
 	http.Handle("/", http.FileServer(http.Dir(".")))
-	// for _, path := range []string{"lib", "js"} {
-	// 	http.Handle("/"+path+"/", http.StripPrefix("/"+path+"/", http.FileServer(http.Dir(path))))
-	// }
 	http.ListenAndServe(":8080", nil)
 }
